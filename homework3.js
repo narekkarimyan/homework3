40ffb9ce268c364771c83b1fb5023b55ea7e4138
naroyerevan <narekkarimyan@gmail.com>
2017-09-19 17:06:51 +0400

    second commit

---
+++ homework3.js
@@ -0,0 +1,54 @@
+const meanCalculator = function (a, b, c, d) {
+	return const mean = (a+b+c+d)/4;
+};
+
+const helloBuilder = function () {
+	return "Hi AUA";
+};
+
+const multiplier = function (a, b, c) {
+	const result = a * b * c;
+};
+
+const fullName = function (firstName, lastName) {
+	return firstName + " " + lastName;
+};
+
+const longestString = function (string1, string2, string3) {
+	if (string1.length > string2.length && string1.length > string3.length) {
+		const result = string1;
+	}
+	else if (string2.length > string1.length && string2.length > string3.length) {
+		result = string2;
+	}
+	else if (string3.length > string1.length && string3.length > string2.length) {
+		result = string3;
+	}
+	else {
+		return "There is no string that is the longest";
+	}
+};
+
+const comparator = function (num1, num2) {
+	if (num1 === num2) {
+		return 0;
+	}
+	else if (num1 > num2) {
+		return 1;
+	}
+	else {
+		return -1;
+	}
+};
+
+const firstTruethy = function (a, b , c) {
+	if (a) {
+		return a;
+	}	
+	else if (b) {
+		return b;
+	}
+	else {
+		return c;
+	}
+};
